77701826ad2ba6fb34f1e7f28e2a2b2ba7c2874a
AsemK <asem.khattab@gmail.com>
2018-05-11 15:01:46 +0300

    revised and documented the CSPSolver class

--- csp.py
+++ csp.py
@@ -44,14 +44,30 @@ class CSP:
 
 
 class CSPSolver:
-    def __init__(self, csp, convert_solution=lambda x: x):
+    """
+    An implementation of the backtracking search algorithm as described in
+    the book "AI, a Modern Approach", ed. 2, ch. 6.
+    """
+    def __init__(self, csp):
+        """
+        :param csp: a constraint satisfaction problem object that provides the function
+        indicated in CSP abstract class.
+        """
+        self.iterations = 0
         self.csp = csp
         self.vars = csp.get_variables()
         self.domains = csp.get_domains()
+        # the function that selects the next variable to be assigned.
         self.select_unassigned_variable = self.get_unassigned_mrv
-        self.convert_solution = convert_solution
 
     def forward_check(self, assignments, domains):
+        """
+        A function that should be called at the begging of each search iteration to
+        eliminate assignments inconsistent with the current assignment from the search space.
+        :return: a new domains dictionary where all the assignments are consistent with
+        the input assignments. Returns None if one variable doesn't have any assignment
+        consistent with the input assignments.
+        """
         new_domains = dict()
         self.csp.set_current_assignments(assignments)
         for var in set(domains.keys()) - set(assignments.keys()):
@@ -65,14 +81,24 @@ class CSPSolver:
 
     @staticmethod
     def get_unassigned_mrv(assignments, domains):
-        return min([(len(domains[var]), var) for var in set(domains.keys()) - set(assignments.keys())])[1]
+        """
+        Selects the next variable to be assigned by the minimum remaining values heuristic
+        :return: the variable with the minimum remaining values
+        """
+        return min([(len(domains[var]), var)
+                    for var in set(domains.keys()) - set(assignments.keys())])[1]
 
     def back_tracking_search(self, assignments, domains):
+        """
+        The recursive back-tracking search method
+        :return: the first found solution or None if there is not any. The solution is
+        returned as a dictionary containing variables and the corresponding values.
+        """
         var = self.select_unassigned_variable(assignments, domains)
         if len(assignments) == len(self.vars)-1:
             assignments[var] = domains[var][0]
             return assignments
-        self.iterations += 1
+        self.iterations += 1  # search iterations counter
         for val in domains[var]:
             assignments[var] = val
             new_domains = self.forward_check(assignments, domains)
@@ -83,15 +109,12 @@ class CSPSolver:
         return None
 
     def get_solution(self):
-        self.iterations = 0
+        self.iterations = 0  # reset iterations counter
+        # ensure that each variable has a non-empty domain
         for shape in self.domains:
             if not self.domains[shape]:
                 return None
-        solution = self.back_tracking_search(dict(), self.domains)
-        if solution:
-            return self.convert_solution(solution)
-        else:
-            return None
+        return self.back_tracking_search(dict(), self.domains)
 
     def get_search_iterations(self):
         return self.iterations
