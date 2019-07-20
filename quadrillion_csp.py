564a19700d658c7d7d54d7fada393c8a4a870efd
asemk <asem.khattab@gmail.com>
2019-07-20 13:32:40 +0200

    added a generator for connected dots

--- quadrillion_csp.py
+++ quadrillion_csp.py
@@ -20,25 +20,26 @@ class QuadrillionCSPAdapter(CSP):
         return self._domains
 
     def set_current_assignments(self, assignments):
-        self._empty_grids_dots = self._quadrillion.released_empty_grids_dots
-        for shape, config in assignments.items():
-            shape.config = config
-            self._empty_grids_dots -= shape
+        self._current_empty_grids_dots = self._empty_grids_dots.copy()
+        for dots in assignments.values():
+            self._current_empty_grids_dots -= dots
 
     def is_consistent_assignment(self, assignment):
-        shape, config = assignment
-        shape.config = config
-        return shape <= self._empty_grids_dots and is_connected(self._empty_grids_dots - shape)
+        shape, dots = assignment
+        return dots <= self._current_empty_grids_dots\
+               and is_connected(self._current_empty_grids_dots - dots)
 
     def _extract_domains(self):
+        self.set_current_assignments(dict())
         domains = dict()
         square_dots = self._get_smallest_square_over_dots(self._empty_grids_dots)
         for variable in self._variables:
             domain = set()
             for loc in square_dots:
                 for config in variable.get_unique_configs_at(loc):
-                    if self.is_consistent_assignment((variable, config)):
-                        domain.add(config)
+                    dots = frozenset(variable.configured(config))
+                    if self.is_consistent_assignment((variable, dots)):
+                        domain.add(dots)
             domains[variable] = domain
         return domains
 
@@ -54,42 +55,43 @@ class QuadrillionCSPAdapter(CSP):
         solution = csp_solver.get_solution()
         self.iterations = csp_solver.iterations
         for shape in self._variables:
-            shape.config = solution[shape]
+            shape.set_dots(solution[shape])
         self._quadrillion.release()
 
 
-def is_connected(dot_set):
-    closed = set()
-    set_len = len(dot_set)
-    for dot in dot_set:
-        if dot not in closed:
-            connected = small_bfs(dot_set, closed, dot)
-            if not valid_pos_set_component(set_len, connected):
-                return False
+def is_connected(empty_dots):
+    nr_empty_dots = len(empty_dots)
+    for connected_dots_set in connected_dots_sets(empty_dots):
+        if not valid_pos_set_component(nr_empty_dots, len(connected_dots_set)):
+            return False
     return True
 
 
-def small_bfs(dot_set, closed, dot):
-    fringe = []
-    fringe.insert(0, dot)
-    closed.add(dot)
-    connected = 1
-    while fringe:
-        s = fringe.pop()
-        successors = [(s[0]+1, s[1]), (s[0]-1, s[1]), (s[0], s[1]+1), (s[0], s[1]-1)]
-        for suc in successors:
-            if (suc in dot_set) and (suc not in closed):
-                fringe.insert(0, suc)
-                closed.add(suc)
-                connected += 1
-    return connected
+def connected_dots_sets(dots_set):
+    def connected_dots_set_at(dot):
+        connected_dots = {dot}
+        dots_queue =[dot]
+        while dots_queue:
+            y, x = dots_queue.pop()
+            successors = dots_set & ({(y+1, x), (y-1, x), (y, x+1), (y, x-1)} - connected_dots)
+            for dot in successors:
+                connected_dots.add(dot)
+                dots_queue.insert(0, dot)
+        return connected_dots
+    seen_dots = set()
+    for dot in dots_set:
+        if dot in seen_dots: continue
+        connected_dots = connected_dots_set_at(dot)
+        yield connected_dots
+        seen_dots |= connected_dots
 
 
-def valid_pos_set_component(set_len, connected):
-    if ((set_len % 5 == 0 and connected % 5 == 0)
-    or ((set_len % 5) % 4 == 0 and (connected % 5) % 4 == 0)
-    or ((set_len % 5) % 3 == 0 and (connected % 5) % 3 == 0)
-    or ((57 - set_len) % 5 == 0 and (connected%5%4%3 == 0 or ((connected-7)>=0 and (connected-7)%5 == 0)))):
+def valid_pos_set_component(nr_empty_dots, nr_empty_connected_dots):
+    if ((nr_empty_dots % 5 == 0 and nr_empty_connected_dots % 5 == 0)        # one of the variables should have 5 dots
+    or (nr_empty_dots % 5 % 4 == 0 and nr_empty_connected_dots % 5 % 4 == 0) # one of the variables should be four
+    or (nr_empty_dots % 5 % 3 == 0 and nr_empty_connected_dots % 5 % 3 == 0) # one of the variables should be three
+    # one variable should be 4 and one should be three
+    or ((57 - nr_empty_dots) % 5 == 0 and (nr_empty_connected_dots % 5 % 4 % 3 == 0 or ((nr_empty_connected_dots - 7) >= 0 and (nr_empty_connected_dots - 7) % 5 == 0)))):
         return True
     else:
         return False
